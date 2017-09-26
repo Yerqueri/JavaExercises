54b08d55f699f3fe8dfbecd590289c74e17157d1
Yerqueri <yayush13@gmail.com>
2017-09-26 10:22:55 +0530

    collections.md expected

--- com/exercises/PE25.java
+++ com/exercises/PE25.java
@@ -2,18 +2,28 @@ package com.exercises;
 
 public class PE25 {
 	public static void main(String[] args) {
-		String white="WW|";
-		String black="BB|";
-		
-		String[] even = {black,white};
-		String[] odd = {white,black};
-		
-//		String[[]] evenrow=new String[[4]];
-//		String[[]] oddrow=new String[[4]];
-//		for(int i=0,i<4;i++) {
-//			if(i%2==0) {
-//				evenrow[i]=even;
-//			}
-//		}
+		String[][] pattern = new String[8][8];
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i % 2 == 0) {
+					if (j % 2 == 0)
+						pattern[i][j] = "WW";
+					else
+						pattern[i][j] = "BB";
+				} else {
+					if (j % 2 == 0)
+						pattern[i][j] = "BB";
+					else
+						pattern[i][j] = "WW";
+				}
+			}
+		}
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++)
+				System.out.print(pattern[i][j] + "|");
+			System.out.println();
+		}
 	}
 }
